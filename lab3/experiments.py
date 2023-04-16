6135acb7a664d72fc476af44ab32ad00b5d6fa09
Gamsikowy <gamsikowy@gmail.com>
2023-04-16 20:58:40 +0200

    Add local search with list

--- lab3/experiments.py
+++ lab3/experiments.py
@@ -7,11 +7,11 @@ import time
 from candidates import candidates_moves
 from moves_rate import moves_rate
 
-def run_experiments(N=1):
+def run_experiments(N=100):
     instances = ['kroA200.tsp', 'kroB200.tsp']
     algorithms = [(candidates_moves, 'Ruchy kandydatckie')]
     algorithms = [(moves_rate, 'Lokalne przeszukiwanie')]
-    with open('results_lab3_moves.txt', 'w') as results_file:
+    with open('results_lab3_lokalne_przeszukiwanie.txt', 'w') as results_file:
         for algorithm, name_algorithm in algorithms:
             for instance in instances:
 
@@ -25,22 +25,19 @@ def run_experiments(N=1):
                     distance_matrix, nodes = read_file(instance)
                     
                     cycles = make_random_solution(len(nodes))
-                    
                     cycles = algorithm(cycles, distance_matrix)
-                        
+                    
                     end = time.time()
                     times.append(end - start)
-
                     results_in_scope.append(calc_cycles_length(distance_matrix, cycles))
 
                     if best_cycle_len > calc_cycles_length(distance_matrix, cycles):
                         best_cycle_len = calc_cycles_length(distance_matrix, cycles)
                         best_cycle = cycles
-
                 
                 results_file.write(f"{name_algorithm}\t{instance}\t = {np.mean(results_in_scope)} ({np.min(results_in_scope)} - {np.max(results_in_scope)}) | {np.mean(times)} ({np.min(times)} - {np.max(times)})\n")
                 save_result(f"{name_algorithm}-{instance.split('.')[0]}", best_cycle[0], best_cycle[1], nodes)
 
 
 if __name__ == '__main__':
-    run_experiments(10)
+    run_experiments(100)
--- lab3/moves_rate.py
+++ lab3/moves_rate.py
@@ -1,167 +1,204 @@
-# Ocena ruchów z poprzednich iteracji
-from algorithm_utils import get_node_pair, get_between_cycles_node_pair, delta_between_cycles_node_exchange, delta_inside_cycle_edge_exchange, exchange_edge_in_cycle, exchange_nodes_between_cycles
+from time import time
+from copy import deepcopy
+import pandas as pd
+import numpy as np
 
-def get_node_pair_delta(cycles, distance_matrix):
-    possibilities = []
-    for cycle_idx, cycle in enumerate(cycles):
-        for node1 in range(len(cycle)):
-            for node2 in range(node1 + 1, len(cycle)):
-                delta = delta_inside_cycle_edge_exchange(distance_matrix, cycles[cycle_idx], [cycle[node1], cycle[node2]])
-                node1_next = 0 if node1+1 >= len(cycle) else node1+1
-                node2_next = 0 if node2+1 >= len(cycle) else node2+1
-                # if delta > 0: possibilities.append(([cycle[node1], cycle[node2]], delta_inside_cycle_edge_exchange, cycle_idx, delta, [cycle[node1_next]], [cycle[node2_next]]))
-                if delta < 0: 
-                    possibilities.append(([cycle[node1], cycle[node2]], delta_inside_cycle_edge_exchange, cycle_idx, delta, [cycle[node1_next]], [cycle[node2_next]]))
-    return possibilities
+inside_cycle_edge_exchange = 0
+between_cycles_node_exchange = 1
 
-def get_between_cycles_node_pair_delta(cycles, distance_matrix):
-    possibilities = []
-    for node1 in range(len(cycles[0])):
-        for node2 in range(len(cycles[1])):
-            delta = delta_between_cycles_node_exchange(distance_matrix, cycles, [cycles[0][node1], cycles[1][node2]])
-            
-            next1 = cycles[0][0] if node1+1 >= len(cycles[0]) else cycles[0][node1+1]
-            next2 = cycles[1][0] if node2+1 >= len(cycles[1]) else cycles[1][node2+1]
+def distance(a, b):
+    return np.round(np.sqrt(np.sum((a - b) ** 2)))
 
-            prev1 = cycles[0][node1-1]
-            prev2 = cycles[1][node2-1]
+def get_node(cycles, a):
+    i = None
+    try:
+        i = cycles[0].index(a)
+    except:
+        i = None
 
-            if delta < 0: 
-                possibilities.append(([cycles[0][node1], cycles[1][node2]], delta_between_cycles_node_exchange, None, delta, [prev1, next1], [prev2, next2]))
-            # if delta > 0: possibilities.append(([cycles[0][node1], cycles[1][node2]], delta_between_cycles_node_exchange, None, delta, [prev1, next1], [prev2, next2]))
-    return possibilities
+    if i is not None: return 0, i
+    i = None
+    try:
+        i = cycles[1].index(a)
+    except:
+        i = None
 
-def get_prev_next(cycles, node):
-    for cycle in cycles:
-        if node in cycle:
-            idx = cycle.index(node)
-            prev_n = cycle[idx-1]
-            next_n = cycle[0] if idx+1 >= len(cycle) else cycle[idx+1]
-            return prev_n, next_n
+    if i is not None: return 1, i
+    assert False, 'Node must be in one of the cycles'
 
-def new_possibilities(possibility, cycles, distance_matrix):
-    _, action, _, _, _, _ = possibility
-    if action == delta_inside_cycle_edge_exchange:
-        return get_node_pair_delta(cycles, distance_matrix)
-    elif action == delta_between_cycles_node_exchange:
-        return get_between_cycles_node_pair_delta(cycles, distance_matrix)
-    else:
-        ValueError("Fatal error")
+def reverse(xs, i, j):
+    n = len(xs)
+    d = (j - i) % n
+    for k in range(abs(d)//2+1):
+        a, b = (i + k) % n, (i + d - k) % n
+        xs[a], xs[b] = xs[b], xs[a]
 
-def new_possibilities_2(possibility, cycles, distance_matrix):
-    nodes, action, _, _, _, _ = possibility
-    new_moves = []
-    if action == delta_inside_cycle_edge_exchange:
-
-        return get_node_pair_delta(cycles, distance_matrix)
-    elif action == delta_between_cycles_node_exchange:
-        for x in cycles[0]:
-            delta = delta_between_cycles_node_exchange(distance_matrix, cycles, [x, nodes[1]])
-            if delta < 0:
-                node1 = x
-                node2 = nodes[1]
-                
-                next1 = cycles[0][0] if node1+1 >= len(cycles[0]) else cycles[0][node1+1]
-                next2 = (cycles[0].index(node2)+1)%len(cycles[0])
-
-                prev1 = cycles[0][node1-1]
-                prev2 = cycles[1][node2-1]
-                new_moves.append(([x, nodes[1]], delta_between_cycles_node_exchange, None, delta, [prev1, next1], [prev2, next2]))
-        for x in cycles[1]:
-            delta = delta_between_cycles_node_exchange(distance_matrix, cycles, [nodes[0], x])
-            if delta < 0:
-                next1 = cycles[1][0] if node1+1 >= len(cycles[0]) else cycles[0][node1+1]
-                next2 = cycles[1][0] if node2+1 >= len(cycles[1]) else cycles[1][node2+1]
-
-                prev1 = cycles[0][node1-1]
-                prev2 = cycles[1][node2-1]
-                new_moves.append(([nodes[0], x], delta_between_cycles_node_exchange, None, delta, [cycles[0][cycles[0].index(nodes[0])-1], cycles[0][(cycles[0].index(nodes[0])+1)%len(cycles[0])]], [cycles[1][cycles[1].index(x)-1], cycles[1][(cycles[1].index(x)+1)%len(cycles[1])]]))
-        # return get_between_cycles_node_pair_delta(cycles, distance_matrix
-        return new_moves
-    else:
-        ValueError("Fatal error")
-
-
-def check_all_edges(cycles, a, b):
-    for i in range(2):
-        status = check_edge(cycles[i], a, b)
-        if status != 0: return i, status
-    return None, 0
-
-def check_edge(cycle, a, b):
+def check_edges(cycle, node1, node2):
     for i in range(len(cycle) - 1):
-        x, y = cycle[i], cycle[i+1]
-        if (a, b) == (x, y): return +1
-        if (a, b) == (y, x): return -1
-
+        x, y = cycle[i], cycle[i + 1]
+        if (node1, node2) == (x, y):
+            return 1
+        if (node1, node2) == (y, x):
+            return -1
+        
     x, y = cycle[-1], cycle[0]
-    if (a, b) == (x, y): return +1
-    if (a, b) == (y, x): return -1
+    if (node1, node2) == (x, y):
+        return 1
+    if (node1, node2) == (y, x):
+        return -1
     return 0
 
+def delta_edge_exchange(cities, cycle, i, j):
+    size = len(cycle)
+    nodes = cycle[i], cycle[(i + 1) % size], cycle[j], cycle[(j + 1) % size]
+
+    return (delta_inside_cycle_edge_exchange(cities, *nodes), *nodes)
+
+def check_all_edges(cycles, node1, node2):
+    for i in range(2):
+        status = check_edges(cycles[i], node1, node2)
+        if status != 0:
+            return i, status
+    return None, 0
+
+def delta_between_cycles_node_exchange(dist_mat, x1, y1, z1, x2, y2, z2):
+    return dist_mat[x1][y2] + dist_mat[z1][y2] - dist_mat[x1][y1] - dist_mat[z1][y1] + dist_mat[x2][y1] + dist_mat[z2][y1] - dist_mat[x2][y2] - dist_mat[z2][y2]
+
+def exchange_nodes_between_cycles(cities, cycles, cyc1, i, cyc2, j):
+    cycle1, cycle2 = cycles[cyc1], cycles[cyc2]
+    n = len(cycle1)
+    m = len(cycle2)
+
+    x1, y1, z1 = cycle1[(i - 1) % n], cycle1[i], cycle1[(i + 1) % n]
+    x2, y2, z2 = cycle2[(j - 1) % m], cycle2[j], cycle2[(j + 1) % m]
+
+    delta = delta_between_cycles_node_exchange(cities, x1, y1, z1, x2, y2, z2)
+    move = delta, between_cycles_node_exchange, cyc1, cyc2, x1, y1, z1, x2, y2, z2
+    return delta, move
+
+def delta_inside_cycle_edge_exchange(cities, a, b, c, d):
+    if a == d or a == b or a == c or b == c or b == d or c == d:
+        return 1e8
+    return cities[a][c] + cities[b][d] - cities[a][b] - cities[c][d]
+
+def get_starting_possibilities(cities, cycles):
+    moves = []
+    for k in range(2):
+        cycle = cycles[k]
+
+        n = len(cycle)
+        possible_edges = [(i, (i + d) % n) for i in range(n) for d in range(2, n - 1)]
+
+        for i, j in possible_edges:
+            delta, a, b, c, d = delta_edge_exchange(cities, cycle, i, j)
+            if delta < 0:
+                moves.append((delta, inside_cycle_edge_exchange, a, b, c, d))
+
+    possible_nodes = [(i, j) for i in range(len(cycles[0])) for j in range(len(cycles[1]))]
+    for i, j in possible_nodes:
+        delta, move = exchange_nodes_between_cycles(cities, cycles, 0, i, 1, j)
+        if delta < 0:
+            moves.append(move)
+    return moves
+ 
+def make_move(cycles, move):
+    action = move[1]
+    if action == inside_cycle_edge_exchange:
+        _, _, a, _, c, _ = move
+        (cycle1, i) = get_node(cycles, a)
+        (cycle2, j) = get_node(cycles, c)
+        cycle = cycles[cycle1]
+        n = len(cycle)
+        reverse(cycle, (i + 1) % n, j)
+    elif action == between_cycles_node_exchange:
+        _, _, cycle1, cycle2, _, a, _, _, b, _ = move
+        i, j = cycles[cycle1].index(a), cycles[cycle2].index(b)
+        cycles[cycle1][i], cycles[cycle2][j] = cycles[cycle2][j], cycles[cycle1][i]
+
+def check_edge_connection(cycles, possibility):
+    _, _, cycle1, cycle2, x1, y1, z1, x2, y2, z2 = possibility
+    first_cyc1 = check_edges(cycles[cycle1], x1, y1)
+    first_cyc2 = check_edges(cycles[cycle1], y1, z1)
+    second_cyc1 = check_edges(cycles[cycle2], x2, y2)
+    second_cyc2 = check_edges(cycles[cycle2], y2, z2)
+    return first_cyc1, first_cyc2, second_cyc1, second_cyc2
+    
+def new_possibilities(distance_matrix, cycles, possibility):
+    action = possibility[1]
+    possibilities = []
+
+    if action == inside_cycle_edge_exchange:
+        _, _, a, b, c, d = possibility
+
+        if a in cycles[0]:
+            cycle = cycles[0]
+        else:
+            cycle = cycles[1]
+
+        n = len(cycle)
+        possible_edges = [(i, (i + d) % n) for i in range(n) for d in range(2, n - 1)]
+
+        for i, j in possible_edges:
+            delta, a, b, c, d = delta_edge_exchange(distance_matrix, cycle, i, j)
+            if delta < 0:
+                possibilities.append((delta, inside_cycle_edge_exchange, a, b, c, d))
+
+    elif action == between_cycles_node_exchange:
+        _, _, cycle1, cycle2, _, y1, _, _, y2, _ = possibility
+
+        for k in range(len(cycles[cycle2])):
+            delta, possibility = exchange_nodes_between_cycles(distance_matrix, cycles, cycle1, cycles[cycle1].index(y2), cycle2, k)
+            if delta < 0:
+                possibilities.append(possibility)
+
+        for k in range(len(cycles[cycle1])):
+            delta, possibility = exchange_nodes_between_cycles(distance_matrix, cycles, cycle2, cycles[cycle2].index(y1), cycle1, k)
+            if delta < 0:
+                possibilities.append(possibility)
+
+    return possibilities
+
 def moves_rate(cycles, distance_matrix):
-    possibilities = get_node_pair_delta(cycles, distance_matrix)
-    possibilities.extend(get_between_cycles_node_pair_delta(cycles, distance_matrix))
-    possibilities.sort(key = lambda x: x[3])
-    # print(possibilities)
+    cycles = deepcopy(cycles)
+    possibilities = get_starting_possibilities(distance_matrix, cycles)
+    possibilities = sorted(possibilities, key = lambda delta: delta[0])
+    
     while True:
-        new_move = None
-        # print(len(possibilities))
-        to_remove = []
-        if len(possibilities) == 0: break
-        for idx_possibility, possibility in enumerate(possibilities):
-            # print(possibility)
-            nodes, action, cycle_idx, _, connection1, connection2 = possibility
-            if action == delta_inside_cycle_edge_exchange:
-                cyc_idx1, result_1 = check_all_edges(cycles, nodes[0], connection1)
-                cyc_idx2, result_2 = check_all_edges(cycles, nodes[1], connection2)
-                
-                if cyc_idx1 != cyc_idx2 or result_1 == 0 or result_2 == 0:
-                    to_remove.append(idx_possibility)
-                elif result_1 == result_2 == 1:
-                    new_move = possibility  
-                    to_remove.append(idx_possibility)
+        remove_possibilities = []
+        best_move = None
+
+        for possibility_idx, possibility in enumerate(possibilities):
+            action = possibility[1]
+
+            if action == inside_cycle_edge_exchange:
+                _, _, a, b, c, d = possibility
+                (cycle1, same_dir1), (cycle2, same_dir2) = check_all_edges(cycles, a, b), check_all_edges(cycles, c, d)
+
+                if cycle1 != cycle2 or same_dir1 == 0 or same_dir2 == 0:
+                    remove_possibilities.append(possibility_idx)
+                elif same_dir1 == same_dir2 == 1:
+                    remove_possibilities.append(possibility_idx)
+                    best_move = possibility
                     break
-                elif result_1 == result_2 == -1:
-                    new_move = possibility  
-                    to_remove.append(idx_possibility)
+                elif same_dir1 == same_dir2 == -1:
+                    remove_possibilities.append(possibility_idx)
+                    best_move = possibility[0], inside_cycle_edge_exchange, b, a, d, c
+                    break
+
+            elif action == between_cycles_node_exchange:
+                same_dir1, same_dir2, same_dir3, same_dir4 = check_edge_connection(cycles, possibility)
+                
+                if possibility[2] == possibility[3] or same_dir1 == 0 or same_dir2 == 0 or same_dir3 == 0 or same_dir4 == 0:
+                    remove_possibilities.append(possibility_idx)
+                elif same_dir1 == same_dir2 and same_dir3 == same_dir4:
+                    remove_possibilities.append(possibility_idx)
+                    best_move = possibility
                     break
                 
-            elif action == delta_between_cycles_node_exchange:
-                result_1 = check_edge(cycles[0], connection1[0], nodes[0])
-                result_2 = check_edge(cycles[0], nodes[0], connection1[1])
-                result_3 = check_edge(cycles[1], connection2[0], nodes[1])
-                result_4 = check_edge(cycles[1], nodes[1], connection2[1])
-
-                if result_1 == 0 or result_2 == 0 or result_3 == 0 or result_4 == 0:
-                    to_remove.append(idx_possibility)
-                elif result_1 == result_2 and result_3 == result_4:   
-                    new_move = possibility
-            else:
-                ValueError("Fatal error")
-
-        print("TEEEEEEEEEEEST")
-
-        if new_move is None: break
-        
-        possibilities.extend(new_possibilities_2(possibility, cycles, distance_matrix))
-
-        if new_move[1] == delta_inside_cycle_edge_exchange:
-            new_cycle = exchange_edge_in_cycle(cycles[new_move[3]], new_move[0])
-            single_cycle = True
-            best_cycle_idx = new_move[3]
-        elif new_move[1] == delta_between_cycles_node_exchange:
-            new_cycles = exchange_nodes_between_cycles(cycles, new_move[0])
-            single_cycle = False
-            # possibilities.remove(possibility)
-
-        if single_cycle == True:
-            cycles[best_cycle_idx] = new_cycle
-        elif single_cycle == False:
-            cycles = new_cycles
-        # print(*reversed(to_remove))
-        for x in reversed(to_remove):
-            # print(x)
-            possibilities.pop(x)
-
+        if best_move is None: break           
+        for i in reversed(remove_possibilities):
+            del(possibilities[i])
+        make_move(cycles, best_move)
+        new_moves = new_possibilities(distance_matrix, cycles, best_move)
+        possibilities = sorted(list(set(possibilities).union(set(new_moves))), key = lambda x: x[0])
     return cycles
